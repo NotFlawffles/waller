fb879f171dcb19011c8f54774a34e4e73f3e8994
notflawffles <hassenjasim3@gmail.com>
2024-04-08 23:47:00 +0300

    added disappearing ui after 3 seconds of no response

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <ctype.h>
 #include <raylib.h>
-#include <stddef.h>
 #include <malloc.h>
 #include <dirent.h>
 #include <string.h>
@@ -16,121 +15,121 @@ static const char FallbackPath[] = "/home/notflawffles/.fehbg";
 static const char CursorPath[] = "/home/notflawffles/Programming/waller/assets/miku-cursor.png";
 
 typedef struct {
-    const char* path;
-    const char* name;
+    const char *path;
+    const char *name;
 } Wallpaper;
 
-Wallpaper wallpaper_new(const char* path) {
+Wallpaper wallpaper_new(const char *path) {
     return (Wallpaper) {
-	path,
-	path + strlen(WallpapersPath) + 1
+            path,
+            path + strlen(WallpapersPath) + 1
     };
 }
 
 typedef struct {
-    Wallpaper* container;
+    Wallpaper *container;
     size_t size;
 } Wallpapers;
 
-Wallpapers wallpapers_new(Wallpaper* container, size_t size) {
+Wallpapers wallpapers_new(Wallpaper *container, size_t size) {
     return (Wallpapers) {
-	container,
-	size
+            container,
+            size
     };
 }
 
 Wallpapers wallpapers_load() {
-    Wallpaper* container = malloc(sizeof(Wallpaper));
+    Wallpaper *container = malloc(sizeof(Wallpaper));
     size_t size = 0;
-    
-    DIR* wallpapers_directory = opendir(WallpapersPath);
-    struct dirent* dirent;
+
+    DIR *wallpapers_directory = opendir(WallpapersPath);
+    struct dirent *dirent;
 
     while ((dirent = readdir(wallpapers_directory))) {
-	if (*dirent->d_name == '.') {
-	    continue;
-	}
+        if (*dirent->d_name == '.') {
+            continue;
+        }
 
-	char* current_wallpaper_path = malloc(strlen(WallpapersPath) + strlen(dirent->d_name));
-	sprintf(current_wallpaper_path, "%s/%s", WallpapersPath, dirent->d_name);
-	container = realloc(container, sizeof(Wallpaper) * (size + 1));
-	container[size++] = wallpaper_new(current_wallpaper_path);
+        char *current_wallpaper_path = malloc(strlen(WallpapersPath) + strlen(dirent->d_name));
+        sprintf(current_wallpaper_path, "%s/%s", WallpapersPath, dirent->d_name);
+        container = realloc(container, sizeof(Wallpaper) * (size + 1));
+        container[size++] = wallpaper_new(current_wallpaper_path);
     }
 
     return wallpapers_new(container, size);
 }
 
 typedef struct {
-    Texture* container;
+    Texture *container;
     size_t size;
 } Textures;
 
-Textures textures_new(Texture* container, size_t size) {
+Textures textures_new(Texture *container, size_t size) {
     return (Textures) {
-	container,
-	size
+            container,
+            size
     };
 }
 
-Textures textures_load(const Wallpapers* wallpapers) {
-    Texture* container = malloc(sizeof(Texture));
+Textures textures_load(const Wallpapers *wallpapers) {
+    Texture *container = malloc(sizeof(Texture));
     size_t size = 0;
 
     for (size_t index = 0; index < wallpapers->size; index++) {
-	container = realloc(container, sizeof(Texture) * (size + 1));
-	container[size++] = LoadTexture(wallpapers->container[index].path);
+        container = realloc(container, sizeof(Texture) * (size + 1));
+        container[size++] = LoadTexture(wallpapers->container[index].path);
     }
 
     return textures_new(container, size);
 }
 
-void set_wallpaper(const Wallpapers* wallpapers, const size_t* selection) {
-    char* command = malloc(strlen("feh --bg-scale ") + strlen(wallpapers->container[*selection].path));
+void set_wallpaper(const Wallpapers *wallpapers, const size_t *selection) {
+    char *command = malloc(strlen("feh --bg-scale ") + strlen(wallpapers->container[*selection].path));
     sprintf(command, "feh --bg-scale %s", wallpapers->container[*selection].path);
     system(command);
 }
 
 void print_help() {
     puts("-- waller usage --\n\n"
-	 "waller (alone)		launches gui\n"
-	 "waller s[et] <number>	sets wallpaper depending on its order of loading\n"
-	 "waller l[ist]		lists wallpapers\n"
-	 "waller (h[help]|.*)		prints this message");
+         "waller (alone)		launches gui\n"
+         "waller s[et] <number>	sets wallpaper depending on its order of loading\n"
+         "waller l[ist]		lists wallpapers\n"
+         "waller (h[help]|.*)		prints this message");
 }
 
-int command_mode_main(const int argc, char** argv, const Wallpapers* wallpapers, size_t* selection) {
-    const char* command = argv[1];
-    const char* subcommand = argv[2];
+int command_mode_main(const int argc, char **argv, const Wallpapers *wallpapers, size_t *selection) {
+    const char *command = argv[1];
+    const char *subcommand = argv[2];
 
     if (memcmp(command, "s", 1) == 0) {
-	if (subcommand && isdigit(*subcommand)) {
-	    *selection = atoi(subcommand);
-	    set_wallpaper(wallpapers, selection);
-	} else {
-	    puts("waller error: expected a numeric argument");
-	}
+        if (subcommand && isdigit(*subcommand)) {
+            *selection = atoi(subcommand);
+            set_wallpaper(wallpapers, selection);
+        } else {
+            puts("waller error: expected a numeric argument");
+        }
     } else if (memcmp(command, "l", 1) == 0) {
-	for (size_t index = 0; index < wallpapers->size; index++) {
-	    puts(wallpapers->container[index].name);
-	}
+        for (size_t index = 0; index < wallpapers->size; index++) {
+            puts(wallpapers->container[index].name);
+        }
     } else if (memcmp(command, "h", 1) == 0) {
-	print_help();
+        print_help();
     } else {
-	print_help();
+        print_help();
     }
 
     return 0;
 }
 
-void fallback(const Wallpapers* wallpapers, size_t* selection) {
-    FILE* fehbg = fopen(FallbackPath, "r");
+void fallback(const Wallpapers *wallpapers, size_t *selection) {
+    FILE *fehbg = fopen(FallbackPath, "r");
 
     if (!fehbg) {
-	return;
+        return;
     }
 
-    const char* to_skip = "#!/bin/sh\nfeh --no-fehbg --bg-scale '";
-    char* match = malloc(255);
+    const char *to_skip = "#!/bin/sh\nfeh --no-fehbg --bg-scale '";
+    char *match = malloc(255);
 
     fread(match, 1, strlen(to_skip), fehbg);
     memset(match, 0, strlen(to_skip));
@@ -146,10 +145,10 @@ void fallback(const Wallpapers* wallpapers, size_t* selection) {
     fread(match, 1, end - start - 1, fehbg);
 
     for (size_t index = 0; index < wallpapers->size; index++) {
-	if (memcmp(wallpapers->container[index].path, match, strlen(wallpapers->container[index].path)) == 0) {
-	    *selection = index;
-	    break;
-	}
+        if (memcmp(wallpapers->container[index].path, match, strlen(wallpapers->container[index].path)) == 0) {
+            *selection = index;
+            break;
+        }
     }
 }
 
@@ -160,23 +159,24 @@ typedef struct {
 
 Cursor cursor_new(Texture texture, const Vector2 position) {
     return (Cursor) {
-	texture,
-	position
+            texture,
+            position
     };
 }
 
-void cursor_set_position(Cursor* cursor, const Vector2 position) {
+void cursor_set_position(Cursor *cursor, const Vector2 position) {
     cursor->position = position;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     const Wallpapers wallpapers = wallpapers_load();
     size_t selection = 0;
 
     if (argc > 1) {
-	return command_mode_main(argc, argv, &wallpapers, &selection);
+        return command_mode_main(argc, argv, &wallpapers, &selection);
     }
 
+    SetConfigFlags(FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_RESIZABLE);
     InitWindow(WindowWidth, WindowHeight, WindowTitle);
     SetTargetFPS(WindowFramesPerSecond);
     HideCursor();
@@ -188,32 +188,50 @@ int main(int argc, char** argv) {
 
     Cursor cursor = cursor_new(LoadTexture(CursorPath), GetMousePosition());
 
+    unsigned short counter = 0;
+
     while (!WindowShouldClose()) {
-	BeginDrawing();
+        BeginDrawing();
 
-	if (IsKeyPressed(KEY_RIGHT) && selection < wallpapers.size - 1) {
-	    selection++;
-	} else if (IsKeyPressed(KEY_LEFT) && selection > 0) {
-	    selection--;
-	}
+        if (GetKeyPressed() || (GetMouseX() != cursor.position.x && GetMouseY() != cursor.position.y)) {
+            counter = 0;
+        }
 
-	if (IsKeyPressed(KEY_ENTER)) {
-	    set_wallpaper(&wallpapers, &selection);
-	    break;
-	}
+        if (IsKeyPressed(KEY_RIGHT) && selection < wallpapers.size - 1) {
+            selection++;
+        } else if (IsKeyPressed(KEY_LEFT) && selection > 0) {
+            selection--;
+        }
 
-	cursor_set_position(&cursor, GetMousePosition());
+        if (IsKeyPressed(KEY_ENTER)) {
+            set_wallpaper(&wallpapers, &selection);
+            break;
+        }
 
- 	ClearBackground(BLACK);
-	DrawTexture(textures.container[selection], 0, 0, WHITE);
-	DrawText("<", 10, GetScreenHeight() / 2.0 - 30 / 2.0, 30, WHITE);
-	DrawText(">", GetScreenWidth() - 5 - 30 / 2.0, GetScreenHeight() / 2.0 - 30 / 2.0, 30, WHITE);
+        cursor_set_position(&cursor, GetMousePosition());
 
-	const Vector2 text_size = MeasureTextEx(GetFontDefault(), wallpapers.container[selection].name, 30, 0);
-	DrawText(wallpapers.container[selection].name, GetScreenWidth() / 2.0 - text_size.x / 2.0, text_size.y / 2.0, 30, WHITE);
+        ClearBackground(BLACK);
 
-	DrawTexture(cursor.texture, cursor.position.x, cursor.position.y, WHITE);
-	EndDrawing();
+        DrawTexturePro(textures.container[selection],
+                        (Rectangle) {0, 0, textures.container[selection].width, textures.container[selection].height}, (Rectangle) {0, 0, GetRenderWidth(), GetRenderHeight()}, (Vector2) {0, 0}, 0, WHITE);
+
+        unsigned short ui_disappearance_frames_delay = GetFPS() * 3;
+
+        if (counter < ui_disappearance_frames_delay) {
+            DrawText("<", 10, GetRenderHeight() / 2.0 - 30 / 2.0, 30, WHITE);
+            DrawText(">", GetRenderWidth() - 5 - 30 / 2.0, GetRenderHeight() / 2.0 - 30 / 2.0, 30, WHITE);
+
+            const Vector2 text_size = MeasureTextEx(GetFontDefault(), wallpapers.container[selection].name, 30, 0);
+            DrawText(wallpapers.container[selection].name, GetRenderWidth() / 2.0 - text_size.x / 2.0,
+                     text_size.y / 2.0,
+                     30, WHITE);
+
+            DrawTexture(cursor.texture, cursor.position.x, cursor.position.y, WHITE);
+        }
+
+        EndDrawing();
+
+        counter++;
     }
 
     CloseWindow();
